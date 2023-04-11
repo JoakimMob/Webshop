709983f97ca811bfa8ccfb5a40560ae4324adcca
joakim <joakimmoberg@hotmail.se>
2023-04-11 15:16:10 +0200

    added contructor in customer

--- src/main/java/com/example/webshop/business/Customer.java
+++ src/main/java/com/example/webshop/business/Customer.java
@@ -5,6 +5,7 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Entity
@@ -23,6 +24,19 @@ public class Customer {
     private List<CustomerOrder> customerOrders;
 
     public Customer() {
+    }
+
+    public Customer(Long id, String email, String password, List<CustomerOrder> customerOrders) {
+        this.id = id;
+        this.email = email;
+        this.password = password;
+        this.customerOrders = customerOrders;
+    }
+
+    public Customer(String email, String password) {
+        this.email = email;
+        this.password = password;
+        customerOrders= new ArrayList<>();
     }
 
     public Long getId() {
