f8f0f1a054eb2e2e3c66368435320a795259ae22
joakim <joakimmoberg@hotmail.se>
2023-04-12 14:48:36 +0200

    login started, in service and controller

--- src/main/java/com/example/webshop/business/WebShopService.java
+++ src/main/java/com/example/webshop/business/WebShopService.java
@@ -1,7 +1,23 @@
 package com.example.webshop.business;
 
+import com.example.webshop.data.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.context.annotation.SessionScope;
+
+import java.util.List;
 
 @Service
+@SessionScope
 public class WebShopService {
+
+    @Autowired
+    CustomerRepository customerRepository;
+    Customer customer;
+
+    public Customer login(String loginUser, String password) {
+        List<Customer> customerList = customerRepository.findByEmailAndPassword(loginUser,password);
+        customer = customerList.get(1);
+        return customer;
+    }
 }
--- src/main/java/com/example/webshop/ui/WebShopController.java
+++ src/main/java/com/example/webshop/ui/WebShopController.java
@@ -1,4 +1,22 @@
 package com.example.webshop.ui;
 
+import com.example.webshop.business.WebShopService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
 public class WebShopController {
+    @Autowired
+    WebShopService webShopService;
+
+    @PostMapping("/login")
+    public String login(@RequestParam String loginUser, @RequestParam String password, Model m){
+        m.addAttribute("person", webShopService.login(loginUser,password));
+        return "product";
+    }
+
+
 }
